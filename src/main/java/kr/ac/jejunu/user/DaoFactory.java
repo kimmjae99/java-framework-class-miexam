7d16fb81d4e1fc0b4927d8d91c0d28fe4f88e911
kimmjae99 <kimmjae99@gmail.com>
2023-04-21 16:34:28 +0900

    4번 문제

--- src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -1,4 +1,12 @@
 package kr.ac.jejunu.user;
 
+
 public class DaoFactory {
+    public UserDao getUserDao() {
+        return new UserDao(getConnectionMaker());
+    }
+
+    private static JejuConnectionMaker getConnectionMaker() {
+        return new JejuConnectionMaker();
+    }
 }
--- src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -8,14 +8,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "김민재";
         String password = "1111";
 
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+//        ConnectionMaker connectionMaker = new JejuConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
@@ -31,8 +35,8 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
 
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
 
         User insertedUser = userDao.get(user.getId());
@@ -42,38 +46,38 @@ public class UserDaoTests {
 
     }
 
-    @Test
-    public void getHalla() throws SQLException, ClassNotFoundException {
-        Long id = 1l;
-        String name = "김민재";
-        String password = "1111";
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-
-        User user = userDao.get(id);
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
-
-    @Test
-    public void insertHalla() throws SQLException, ClassNotFoundException {
-        String name = "Aiden";
-        String password = "1234";
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-
-        userDao.insert(user);
-
-        User insertedUser = userDao.get(user.getId());
-
-        assertThat(insertedUser.getName(), is(name));
-        assertThat(insertedUser.getPassword(), is(password));
-
-    }
+//    @Test
+//    public void getHalla() throws SQLException, ClassNotFoundException {
+//        Long id = 1l;
+//        String name = "김민재";
+//        String password = "1111";
+//
+//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+//
+//        User user = userDao.get(id);
+//        assertThat(user.getId(), is(id));
+//        assertThat(user.getName(), is(name));
+//        assertThat(user.getPassword(), is(password));
+//    }
+//
+//    @Test
+//    public void insertHalla() throws SQLException, ClassNotFoundException {
+//        String name = "Aiden";
+//        String password = "1234";
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//
+//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
+//        UserDao userDao = new UserDao(connectionMaker);
+//
+//        userDao.insert(user);
+//
+//        User insertedUser = userDao.get(user.getId());
+//
+//        assertThat(insertedUser.getName(), is(name));
+//        assertThat(insertedUser.getPassword(), is(password));
+//
+//    }
 }
